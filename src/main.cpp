b81ea6e3e031734b9f0b4ea37a485776dadbb9fb
jfjdjdndhh-ux <jfjdjdndhh@gmail.com>
2026-06-10 02:19:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
+#include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
     void startGame() {
         PlayLayer::startGame();
-        this->m_player1->death(nullptr);
+        this->m_player1->playDeathEffect();
     }
 };
