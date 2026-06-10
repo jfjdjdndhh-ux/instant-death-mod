ba32b6584e52d6ae8a6aaea357e7955270185d21
jfjdjdndhh-ux <jfjdjdndhh@gmail.com>
2026-06-10 04:32:18 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/CCScheduler.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 using namespace geode::prelude;
 
+// ===== ИНФО НОУКЛИПА =====
 class NoclipInfo : public CCNode {
 public:
-    CCLabelBMFont* m_deaths;
-    CCLabelBMFont* m_acc;
+    CCLabelTTF* m_deaths;
+    CCLabelTTF* m_acc;
 
     static NoclipInfo* create() {
         auto ret = new NoclipInfo();
@@ -24,16 +26,20 @@ public:
         if (!CCNode::init()) return false;
         auto win = CCDirector::get()->getWinSize();
 
-        m_deaths = CCLabelBMFont::create("Deaths: 0", "chatFont.fnt");
-        m_deaths->setScale(0.45f);
+        auto bg = CCLayerColor::create({0,0,0,120}, 120, 30);
+        bg->setPosition({0, win.height - 58});
+        this->addChild(bg);
+
+        m_deaths = CCLabelTTF::create("Deaths: 0", "Arial", 9);
         m_deaths->setAnchorPoint({0, 0.5f});
-        m_deaths->setPosition({5, win.height - 60});
+        m_deaths->setPosition({4, win.height - 47});
+        m_deaths->setColor({255, 100, 100});
         this->addChild(m_deaths);
 
-        m_acc = CCLabelBMFont::create("Acc: 100.0%", "chatFont.fnt");
-        m_acc->setScale(0.45f);
+        m_acc = CCLabelTTF::create("Acc: 100.0%", "Arial", 9);
         m_acc->setAnchorPoint({0, 0.5f});
-        m_acc->setPosition({5, win.height - 73});
+        m_acc->setPosition({4, win.height - 58});
+        m_acc->setColor({100, 255, 100});
         this->addChild(m_acc);
 
         this->schedule(schedule_selector(NoclipInfo::tick), 0.1f);
@@ -46,6 +52,7 @@ public:
     }
 };
 
+// ===== ХУКИ =====
 class $modify(MyPlayLayer, PlayLayer) {
     struct Fields {
         int totalFrames = 0;
@@ -78,6 +85,12 @@ class $modify(MyPlayLayer, PlayLayer) {
 
         addMegaHackButton(this);
         this->addChild(NoclipInfo::create(), 999);
+
+        // Instant Complete
+        if (g_instantComplete) {
+            this->scheduleOnce(
+                schedule_selector(MyPlayLayer::doInstantComplete), 0.3f);
+        }
     }
 
     void update(float dt) {
@@ -88,14 +101,20 @@ class $modify(MyPlayLayer, PlayLayer) {
     void killMe(float) {
         PlayLayer::destroyPlayer(this->m_player1, nullptr);
     }
+
+    void doInstantComplete(float) {
+        this->m_player1->setPositionX(99999.f);
+    }
 };
 
+// ===== SPEEDHACK =====
 class $modify(CCScheduler) {
     void update(float dt) {
         CCScheduler::update(dt * g_speed);
     }
 };
 
+// ===== КНОПКА В ГЛАВНОМ МЕНЮ =====
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
